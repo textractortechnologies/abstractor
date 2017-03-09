d05d8f27bfa904ba28bbfa70aef40c16d0693e1b
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2017-03-09 10:05:19 -0500

    close #53 Allow dictionary items to be marked as case sensitive.

--- spec/requests/abstractor/abstractor_abstraction_schema_controller_spec.rb
+++ spec/requests/abstractor/abstractor_abstraction_schema_controller_spec.rb
@@ -14,8 +14,8 @@ describe Abstractor::AbstractorAbstractionSchemasController, :type => :request d
       abstractor_object_type = Abstractor::AbstractorObjectType.where(value: 'list').first
       abstractor_abstraction_schema = FactoryGirl.create(:abstractor_abstraction_schema, predicate: 'has_some_property', display_name: 'some_property', abstractor_object_type: abstractor_object_type, preferred_name: 'property')
       abstractor_abstraction_schema.abstractor_abstraction_schema_predicate_variants << FactoryGirl.build(:abstractor_abstraction_schema_predicate_variant, value: 'smoperty')
-      abstractor_abstraction_schema.abstractor_object_values << FactoryGirl.build(:abstractor_object_value, value: 'foo', properties: '{  "type":"Rpt", "select_for":"Brain, CNS, and Pituitary" }', vocabulary_code: '8148/0', vocabulary: 'ICD-O-3', vocabulary_version: '2011 Updates to ICD-O-3')
-      FactoryGirl.create(:abstractor_object_value_variant, abstractor_object_value: abstractor_abstraction_schema.abstractor_object_values.first, value: 'boo')
+      abstractor_abstraction_schema.abstractor_object_values << FactoryGirl.build(:abstractor_object_value, value: 'foo', properties: '{  "type":"Rpt", "select_for":"Brain, CNS, and Pituitary" }', vocabulary_code: '8148/0', vocabulary: 'ICD-O-3', vocabulary_version: '2011 Updates to ICD-O-3', case_sensitive: true)
+      FactoryGirl.create(:abstractor_object_value_variant, abstractor_object_value: abstractor_abstraction_schema.abstractor_object_values.first, value: 'boo', case_sensitive: true)
 
       get "/abstractor_abstraction_schemas/#{abstractor_abstraction_schema.id}", {}, accept_json
 
@@ -28,30 +28,30 @@ describe Abstractor::AbstractorAbstractionSchemasController, :type => :request d
       expect(body['abstractor_abstraction_schema']['abstractor_object_type']).to eq 'list'
       expect(body['abstractor_abstraction_schema']['preferred_name']).to eq 'property'
       expect(body['abstractor_abstraction_schema']['predicate_variants']).to eq  [{ 'value' => 'smoperty' }]
-      expect(body['abstractor_abstraction_schema']['object_values']).to eq  [{"value"=>"foo", "properties" => {"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8148/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "object_value_variants"=>[{"value"=>"boo"}]}]
+      expect(body['abstractor_abstraction_schema']['object_values']).to eq  [{"value"=>"foo", "properties" => {"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8148/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "case_sensitive"=> "true", "object_value_variants"=>[{"value"=>"boo", "case_sensitive"=> "true"}]}]
     end
 
     it 'returns an abstraction schema (but not soft deleted object values and soft deleted object value variants)', focus: false do
       abstractor_object_type = Abstractor::AbstractorObjectType.where(value: 'list').first
       abstractor_abstraction_schema = FactoryGirl.create(:abstractor_abstraction_schema, predicate: 'has_some_property', display_name: 'some_property', abstractor_object_type: abstractor_object_type, preferred_name: 'property')
       abstractor_abstraction_schema.abstractor_abstraction_schema_predicate_variants << FactoryGirl.build(:abstractor_abstraction_schema_predicate_variant, value: 'smoperty')
-      abstractor_object_value_1 = FactoryGirl.build(:abstractor_object_value, value: 'foo', properties: '{  "type":"Rpt", "select_for":"Brain, CNS, and Pituitary" }', vocabulary_code: '8148/0', vocabulary: 'ICD-O-3', vocabulary_version: '2011 Updates to ICD-O-3')
+      abstractor_object_value_1 = FactoryGirl.build(:abstractor_object_value, value: 'foo', properties: '{  "type":"Rpt", "select_for":"Brain, CNS, and Pituitary" }', vocabulary_code: '8148/0', vocabulary: 'ICD-O-3', vocabulary_version: '2011 Updates to ICD-O-3', case_sensitive: false)
       abstractor_abstraction_schema.abstractor_object_values << abstractor_object_value_1
-      abstractor_object_value_2 = FactoryGirl.build(:abstractor_object_value, value: 'loo', properties: '{  "type":"Rpt", "select_for":"Brain, CNS, and Pituitary" }', vocabulary_code: '8149/0', vocabulary: 'ICD-O-3', vocabulary_version: '2011 Updates to ICD-O-3')
+      abstractor_object_value_2 = FactoryGirl.build(:abstractor_object_value, value: 'loo', properties: '{  "type":"Rpt", "select_for":"Brain, CNS, and Pituitary" }', vocabulary_code: '8149/0', vocabulary: 'ICD-O-3', vocabulary_version: '2011 Updates to ICD-O-3', case_sensitive: false)
       abstractor_abstraction_schema.abstractor_object_values << abstractor_object_value_2
-      abstractor_object_value_variant = FactoryGirl.create(:abstractor_object_value_variant, abstractor_object_value: abstractor_abstraction_schema.abstractor_object_values.first, value: 'boo')
+      abstractor_object_value_variant = FactoryGirl.create(:abstractor_object_value_variant, abstractor_object_value: abstractor_abstraction_schema.abstractor_object_values.first, value: 'boo', case_sensitive: true)
 
       get "/abstractor_abstraction_schemas/#{abstractor_abstraction_schema.id}", {}, accept_json
       expect(response.status).to be 200
       body = JSON.parse(response.body)
       puts body
-      expect(body['abstractor_abstraction_schema']['object_values']).to eq  [{"value"=>"foo", "properties" => {"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8148/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "object_value_variants"=>[{"value"=>"boo"}]}, {"value"=>"loo", "properties"=>{"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8149/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "object_value_variants"=>[]}]
+      expect(body['abstractor_abstraction_schema']['object_values']).to eq  [{"value"=>"foo", "properties" => {"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8148/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "case_sensitive"=> "false", "object_value_variants"=>[{"value"=>"boo", "case_sensitive"=> "false"}]}, {"value"=>"loo", "properties"=>{"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8149/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "case_sensitive"=>"false", "object_value_variants"=>[]}]
 
       abstractor_object_value_2.soft_delete!
       abstractor_object_value_variant.soft_delete!
       get "/abstractor_abstraction_schemas/#{abstractor_abstraction_schema.id}", {}, accept_json
       body = JSON.parse(response.body)
-      expect(body['abstractor_abstraction_schema']['object_values']).to eq  [{"value"=>"foo", "properties" => {"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8148/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "object_value_variants"=>[]}]
+      expect(body['abstractor_abstraction_schema']['object_values']).to eq  [{"value"=>"foo", "properties" => {"type"=>"Rpt", "select_for"=>"Brain, CNS, and Pituitary"}, "vocabulary_code"=>"8148/0", "vocabulary"=>"ICD-O-3", "vocabulary_version"=>"2011 Updates to ICD-O-3", "case_sensitive"=>"false", "object_value_variants"=>[]}]
     end
   end
 end
