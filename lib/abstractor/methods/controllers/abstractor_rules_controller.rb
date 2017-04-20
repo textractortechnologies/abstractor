7c3930d2127c0df898b1d93a64945b0efb4efb75
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2017-04-19 19:54:26 -0500

    references #54 Order the rules by ID temporarily to force order.

--- lib/abstractor/methods/controllers/abstractor_rules_controller.rb
+++ lib/abstractor/methods/controllers/abstractor_rules_controller.rb
@@ -9,7 +9,7 @@ module Abstractor
 
         def index
           abstractor_subject_ids = params[:abstractor_subject_ids].map(&:to_i)
-          abstractor_rules = Abstractor::AbstractorRule.search_by_abstractor_subjects_ids(abstractor_subject_ids)
+          abstractor_rules = Abstractor::AbstractorRule.search_by_abstractor_subjects_ids(abstractor_subject_ids).order('id ASC')
           respond_to do |format|
             format.json { render json: Abstractor::Serializers::AbstractorRulesSerializer.new(abstractor_rules).as_json }
           end
