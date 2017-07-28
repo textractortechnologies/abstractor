66f406e49fe281f5a3e1e993c5e3a24b074e37ed
Yulia Bushmanova <y.bushmanova@gmail.com>
2017-07-27 20:43:47 -0500

    Added separate controller for abstractor settings. References #71

---
+++ app/controllers/abstractor/abstractor_settings_controller.rb
@@ -0,0 +1,5 @@
+module Abstractor
+  class AbstractorSettingsController < Abstractor::ApplicationController
+    include Abstractor::Methods::Controllers::AbstractorSettingsController
+  end
+end
---
+++ lib/abstractor/methods/controllers/abstractor_settings_controller.rb
@@ -0,0 +1,10 @@
+module Abstractor
+  module Methods
+    module Controllers
+      module AbstractorSettingsController
+        def index
+        end
+      end
+    end
+  end
+end
